ed6c99a3fd3f130207b77f529bc08dc97ece4a2a
rysyx_ <rysyx_@outlook.com>
2022-02-15 20:03:13 +0100

    added .bin

---
+++ .bin/autostart.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Notifications and statusbar
+killall wired
+wired -r &
+killall dwmblocks
+dwmblocks &
+pa-applet &
+
+
+#Nice Ones
+#~/bin/lock.sh &
+#eww daemon &
+
+#Apps
+#picom --experimental-backends &
+nitrogen --restore &
+#xsetroot -cursor_name left_ptr &
+numlockx on &
+#greenclip daemon &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#easyeffects --gapplication-service &
+#pamac-tray &
+
+#Monitor settings
+#xrandr --output HDMI-0 --mode 1440x900 -r 75 --pos 0x180 --rotate normal --output DP-4 --primary --mode 1920x1080 -r 144 --pos 1440x0 --rotate normal &
